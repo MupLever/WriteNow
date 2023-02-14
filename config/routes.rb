b83070924455be9aca56ae17bc67add64ac21497
shlyzhas <8willox8@gmail.com>
2023-02-14 23:21:20 +0300

    modifed routes.rb: added new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'main#index'
 end
