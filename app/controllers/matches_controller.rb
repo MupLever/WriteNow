27d2898ae0489ae7d3699d2bac086e39fc275e2a
shlyzhas <8willox8@gmail.com>
2023-02-15 22:40:02 +0300

    in process of creating chat

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,16 +1,60 @@
 class MatchesController < ApplicationController
+  before_action :before_recieve, only: :show
+  before_action :in_chat_room, only: :show
   before_action :no_authentication, only: %i[new create]
+
   def create; end
   def show
-    
+    @messages = @match.messages
   end
 
   def index
-    matches = Match.all.to_a.select {|elem| elem.users.include?(current_user.id) }
+    @matches = Match.all.to_a.select {|elem| elem.users.include?(current_user.id) }
   end
 
   def destroy
     @match.destroy
     flash[:success] = 'The match was successfully deleted'
   end
+
+  def in_chat_room
+    unless@match.users.include?(current_user.id)
+      redirect_to root_path
+    end
+  end
+  
+  def before_recieve
+    @match = Match.find params[:id]
+  end
 end
+
+# class MatchesController < ApplicationController
+#   before_action :no_authentication, only: %i[new create]
+#   before_action :before_recieve, only: :show
+#   before_action :in_chat_room?, only: :show
+
+#   def create; end
+
+#   def show
+#     @messages = @match.messages
+#   end
+
+#   def index
+#     matches = Match.all.to_a.select {|elem| elem.users.include?(current_user.id) }
+#   end
+
+#   def destroy
+#     @match.destroy
+#     flash[:success] = 'The match was successfully deleted'
+#   end
+
+#   def in_chat_room?
+#     !@match.users.include?(current_user.id)
+#   end
+
+#   def before_recieve
+#     @match = Match.find params[:id]
+#   end
+
+
+# end
