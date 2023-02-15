457961f1dc9ba30249da949b91974b4a63928875
shlyzhas <8willox8@gmail.com>
2023-02-15 22:40:31 +0300

    new migration: added column user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_15_085252) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_15_183053) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_15_085252) do
     t.bigint "match_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["match_id"], name: "index_messages_on_match_id"
   end
 
