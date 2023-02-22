6da0a1dde8ec81175e884200a541d5dc2982014f
shlyzhas <8willox8@gmail.com>
2023-02-22 17:20:44 +0300

    added let in tests

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 RSpec.describe Match, type: :model do
     context "test validation model match" do
+        let(:first_user) { User.create email: "mail@mail.ru", name: "Jpytachok", surname: "Jpytachkov", password: "qwerty" }
+        let(:second_user) { User.create email: "mail@gmail.ru", name: "Jpytachok", surname: "Jpytachkov", password: "qwerty" }
+
         it "should be able to create match" do
-            first_user = User.new email: "mail@mail.ru", name: "Jpytachok", surname: "Jpytachkov", password: "qwerty"
-            second_user = User.new email: "mail@gmail.ru", name: "Jpytachok", surname: "Jpytachkov", password: "qwerty"
             match = Match.new users: [first_user, second_user]
             expect(match.valid?).to eq(true)
         end
 
         it "should be able to create match through like method" do
-            first_user = User.create email: "mail@mail.ru", name: "Jpytachok", surname: "Jpytachkov", password: "qwerty"
-            second_user = User.create email: "mail@gmail.ru", name: "Jpytachok", surname: "Jpytachkov", password: "qwerty"
             first_user.like(second_user)
             expect { second_user.like first_user }.to change { Match.count }.by(1)
         end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,17 +1,16 @@
 require 'rails_helper'
 RSpec.describe Message, type: :model do
     context "test validation model message" do
+        let(:first_user) { User.create email: "mail@mail.ru", name: "Jpytachok", surname: "Jpytachkov", password: "qwerty" }
+        let(:second_user) { User.create email: "mail@gmail.ru", name: "Jpytachok", surname: "Jpytachkov", password: "qwerty" }
+        
         it "ensure content is present" do
-            first_user = User.new email: "mail@mail.ru", name: "Jpytachok", surname: "Jpytachkov", password: "qwerty"
-            second_user = User.new email: "mail@gmail.ru", name: "Jpytachok", surname: "Jpytachkov", password: "qwerty"
             match = Match.new users: [first_user, second_user]
             message = Message.new match: match, user: first_user
             expect(message.valid?).to eq(false)
         end
 
         it "should be able to create message / ensure information is optional" do
-            first_user = User.new email: "mail@mail.ru", name: "Jpytachok", surname: "Jpytachkov", password: "qwerty"
-            second_user = User.new email: "mail@gmail.ru", name: "Jpytachok", surname: "Jpytachkov", password: "qwerty"
             match = Match.new users: [first_user, second_user]
             message = Message.new content: "fdfddfdf", match: match, user: first_user
             expect(message.valid?).to eq(true)
