80f55cbf46d95ce77fb5d8d92e43af1d443376d3
shlyzhas <8willox8@gmail.com>
2023-02-25 21:18:33 +0300

    minlength message = 1

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,5 @@
 class Message < ApplicationRecord
   belongs_to :match
   belongs_to :user
-  validates :content, presence: true,length: { minimum: 2 }
+  validates :content, presence: true,length: { minimum: 1 }
 end
